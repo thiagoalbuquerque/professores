b0b47463b5e576755a792f128acb1f1b9c2d6294
Victor Springer <victorspringer@gmail.com>
2016-06-20 21:57:52 -0300

    total of each line of research in the output

--- src/main/java/controller/OutputController.java
+++ src/main/java/controller/OutputController.java
@@ -5,11 +5,7 @@
  */
 package controller;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import model.ConferenceArticle;
 import model.JournalArticle;
 import model.LineOfResearch;
@@ -66,45 +62,74 @@ public class OutputController {
                     + "\n\n";
         
         for(LineOfResearch lineOfResearch : lineOfResearchList) {
+            
+                Integer totalLineOfResearchDoctoralBankingParticipation = 0;
+                Integer totalLineOfResearchMastersBankingParticipation = 0;
+                Integer totalLineOfResearchUndergraduateBankingParticipation = 0;
+                Integer totalLineOfResearchConcludedDoctoralGuidance = 0;
+                Integer totalLineOfResearchConcludedMastersGuidance = 0;
+                Integer totalLineOfResearchConcludedUndergraduateGuidance = 0;
+                Integer totalLineOfResearchInProgressDoctoralGuidance = 0;
+                Integer totalLineOfResearchInProgressMastersGuidance = 0;
+                Integer totalLineOfResearchInProgressUndergraduateGuidance = 0;
+                Integer totalLineOfResearchA1ConferenceArticle = 0;
+                Integer totalLineOfResearchA2ConferenceArticle = 0;
+                Integer totalLineOfResearchB1ConferenceArticle = 0;
+                Integer totalLineOfResearchB2ConferenceArticle = 0;
+                Integer totalLineOfResearchB3ConferenceArticle = 0;
+                Integer totalLineOfResearchB4ConferenceArticle = 0;
+                Integer totalLineOfResearchCConferenceArticle = 0;
+                Integer totalLineOfResearchNCConferenceArticle = 0;
+                Integer totalLineOfResearchA1JournalArticle = 0;
+                Integer totalLineOfResearchA2JournalArticle = 0;
+                Integer totalLineOfResearchB1JournalArticle = 0;
+                Integer totalLineOfResearchB2JournalArticle = 0;
+                Integer totalLineOfResearchB3JournalArticle = 0;
+                Integer totalLineOfResearchB4JournalArticle = 0;
+                Integer totalLineOfResearchCJournalArticle = 0;
+                Integer totalLineOfResearchNCJournalArticle = 0;
+                
             for(Professor professor : lineOfResearch.getProfessors()) {
+                
+                Integer yearsDoctoralBankingParticipation = professor.getResume().getYearsDoctoralBankingParticipation().size();
+                Integer yearsMastersBankingParticipation = professor.getResume().getYearsMastersBankingParticipation().size();
+                Integer yearsUndergraduateBankingParticipation = professor.getResume().getYearsUndergraduateBankingParticipation().size();
+                Integer yearsConcludedDoctoralGuidance = professor.getResume().getYearsConcludedDoctoralGuidance().size();
+                Integer yearsConcludedMastersGuidance = professor.getResume().getYearsConcludedMastersGuidance().size();
+                Integer yearsConcludedUndergraduateGuidance = professor.getResume().getYearsConcludedUndergratuateGuidance().size();
+                Integer yearsInProgressDoctoralGuidance = professor.getResume().getYearsInProgressDoctoralGuidance().size();
+                Integer yearsInProgressMastersGuidance = professor.getResume().getYearsInProgressMastersGuidance().size();
+                Integer yearsInProgressUndergraduateGuidance = professor.getResume().getYearsInProgressUndergraduateGuidance().size();
+                
                 List<ConferenceArticle> conferenceArticles = professor.getResume().getConferenceArticles();
-                String countA1ConferenceArticle = countConferenceArticleLevel("A1", conferenceArticles);
-                String countA2ConferenceArticle = countConferenceArticleLevel("A2", conferenceArticles);
-                String countB1ConferenceArticle = countConferenceArticleLevel("B1", conferenceArticles);
-                String countB2ConferenceArticle = countConferenceArticleLevel("B2", conferenceArticles);
-                String countB3ConferenceArticle = countConferenceArticleLevel("B3", conferenceArticles);
-                String countB4ConferenceArticle = countConferenceArticleLevel("B4", conferenceArticles);
-                String countCConferenceArticle = countConferenceArticleLevel("C", conferenceArticles);
-                String countNCConferenceArticle = countConferenceArticleLevel("N/C", conferenceArticles);
+                Integer countA1ConferenceArticle = countConferenceArticleLevel("A1", conferenceArticles);
+                Integer countA2ConferenceArticle = countConferenceArticleLevel("A2", conferenceArticles);
+                Integer countB1ConferenceArticle = countConferenceArticleLevel("B1", conferenceArticles);
+                Integer countB2ConferenceArticle = countConferenceArticleLevel("B2", conferenceArticles);
+                Integer countB3ConferenceArticle = countConferenceArticleLevel("B3", conferenceArticles);
+                Integer countB4ConferenceArticle = countConferenceArticleLevel("B4", conferenceArticles);
+                Integer countCConferenceArticle = countConferenceArticleLevel("C", conferenceArticles);
+                Integer countNCConferenceArticle = countConferenceArticleLevel("N/C", conferenceArticles);
 
                 List<JournalArticle> journalArticles = professor.getResume().getJournalArticles();
-                String countA1JournalArticle = countJournalArticleLevel("A1", journalArticles);
-                String countA2JournalArticle = countJournalArticleLevel("A2", journalArticles);
-                String countB1JournalArticle = countJournalArticleLevel("B1", journalArticles);
-                String countB2JournalArticle = countJournalArticleLevel("B2", journalArticles);
-                String countB3JournalArticle = countJournalArticleLevel("B3", journalArticles);
-                String countB4JournalArticle = countJournalArticleLevel("B4", journalArticles);
-                String countCJournalArticle = countJournalArticleLevel("C", journalArticles);
-                String countNCJournalArticle = countJournalArticleLevel("N/C", journalArticles);
+                Integer countA1JournalArticle = countJournalArticleLevel("A1", journalArticles);
+                Integer countA2JournalArticle = countJournalArticleLevel("A2", journalArticles);
+                Integer countB1JournalArticle = countJournalArticleLevel("B1", journalArticles);
+                Integer countB2JournalArticle = countJournalArticleLevel("B2", journalArticles);
+                Integer countB3JournalArticle = countJournalArticleLevel("B3", journalArticles);
+                Integer countB4JournalArticle = countJournalArticleLevel("B4", journalArticles);
+                Integer countCJournalArticle = countJournalArticleLevel("C", journalArticles);
+                Integer countNCJournalArticle = countJournalArticleLevel("N/C", journalArticles);
 
-                String countYearsDoctoralBankingParticipation = String
-                        .valueOf(professor.getResume().getYearsDoctoralBankingParticipation().size());
-                String countYearsMastersBankingParticipation = String
-                        .valueOf(professor.getResume().getYearsMastersBankingParticipation().size());
-                String countYearsUndergraduateBankingParticipation = String
-                        .valueOf(professor.getResume().getYearsUndergraduateBankingParticipation().size());
-                String countYearsConcludedDoctoralGuidance = String
-                        .valueOf(professor.getResume().getYearsConcludedDoctoralGuidance().size());
-                String countYearsConcludedMastersGuidance = String
-                        .valueOf(professor.getResume().getYearsConcludedMastersGuidance().size());
-                String countYearsConcludedUndergraduateGuidance = String
-                        .valueOf(professor.getResume().getYearsConcludedUndergratuateGuidance().size());
-                String countYearsInProgressDoctoralGuidance = String
-                        .valueOf(professor.getResume().getYearsInProgressDoctoralGuidance().size());
-                String countYearsInProgressMastersGuidance = String
-                        .valueOf(professor.getResume().getYearsInProgressMastersGuidance().size());
-                String countYearsInProgressUndergraduateGuidance = String
-                        .valueOf(professor.getResume().getYearsInProgressUndergraduateGuidance().size());
+                String countYearsDoctoralBankingParticipation = String.valueOf(yearsDoctoralBankingParticipation);
+                String countYearsMastersBankingParticipation = String.valueOf(yearsMastersBankingParticipation);
+                String countYearsUndergraduateBankingParticipation = String.valueOf(yearsUndergraduateBankingParticipation);
+                String countYearsConcludedDoctoralGuidance = String.valueOf(yearsConcludedDoctoralGuidance);
+                String countYearsConcludedMastersGuidance = String.valueOf(yearsConcludedMastersGuidance);
+                String countYearsConcludedUndergraduateGuidance = String.valueOf(yearsConcludedUndergraduateGuidance);
+                String countYearsInProgressDoctoralGuidance = String.valueOf(yearsInProgressDoctoralGuidance);
+                String countYearsInProgressMastersGuidance = String.valueOf(yearsInProgressMastersGuidance);
+                String countYearsInProgressUndergraduateGuidance = String.valueOf(yearsInProgressUndergraduateGuidance);
 
                 output += professor.getName() + "\t\t\t"
                         + countA1JournalArticle + "\t\t\t"
@@ -132,10 +157,60 @@ public class OutputController {
                         + countYearsInProgressDoctoralGuidance + "\t\t\t"
                         + countYearsInProgressMastersGuidance + "\t\t\t"
                         + countYearsInProgressUndergraduateGuidance + "\n\n";
+                
+                totalLineOfResearchDoctoralBankingParticipation += yearsDoctoralBankingParticipation;
+                totalLineOfResearchMastersBankingParticipation += yearsMastersBankingParticipation;
+                totalLineOfResearchUndergraduateBankingParticipation += yearsUndergraduateBankingParticipation;
+                totalLineOfResearchConcludedDoctoralGuidance += yearsConcludedDoctoralGuidance;
+                totalLineOfResearchConcludedMastersGuidance += yearsConcludedMastersGuidance;
+                totalLineOfResearchConcludedUndergraduateGuidance += yearsConcludedUndergraduateGuidance;
+                totalLineOfResearchInProgressDoctoralGuidance += yearsInProgressDoctoralGuidance;
+                totalLineOfResearchInProgressMastersGuidance += yearsInProgressMastersGuidance;
+                totalLineOfResearchInProgressUndergraduateGuidance += yearsInProgressUndergraduateGuidance;
+                totalLineOfResearchA1ConferenceArticle += countA1ConferenceArticle;
+                totalLineOfResearchA2ConferenceArticle += countA2ConferenceArticle;
+                totalLineOfResearchB1ConferenceArticle += countB1ConferenceArticle;
+                totalLineOfResearchB2ConferenceArticle += countB2ConferenceArticle;
+                totalLineOfResearchB3ConferenceArticle += countB3ConferenceArticle;
+                totalLineOfResearchB4ConferenceArticle += countB4ConferenceArticle;
+                totalLineOfResearchCConferenceArticle += countCConferenceArticle;
+                totalLineOfResearchNCConferenceArticle += countNCConferenceArticle;
+                totalLineOfResearchA1JournalArticle += countA1JournalArticle;
+                totalLineOfResearchA2JournalArticle += countA2JournalArticle;
+                totalLineOfResearchB1JournalArticle += countB1JournalArticle;
+                totalLineOfResearchB2JournalArticle += countB2JournalArticle;
+                totalLineOfResearchB3JournalArticle += countB3JournalArticle;
+                totalLineOfResearchB4JournalArticle += countB4JournalArticle;
+                totalLineOfResearchCJournalArticle += countCJournalArticle;
+                totalLineOfResearchNCJournalArticle += countNCJournalArticle;
             }
 
-            output += String.valueOf(lineOfResearch.getProfessors().size()) + "\n\n"
-            + "\n\n\n";
+            output += "Total da Linha de Pesquisa '" + lineOfResearch.getName() + "'\t\t\t"
+                + String.valueOf(totalLineOfResearchA1JournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchA2JournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB1JournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB2JournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB3JournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB4JournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchCJournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchNCJournalArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchA1ConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchA2ConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB1ConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB2ConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB3ConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchB4ConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchCConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchNCConferenceArticle) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchDoctoralBankingParticipation) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchMastersBankingParticipation) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchUndergraduateBankingParticipation) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchConcludedDoctoralGuidance) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchConcludedMastersGuidance) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchConcludedUndergraduateGuidance) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchInProgressDoctoralGuidance) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchInProgressMastersGuidance) + "\t\t\t"
+                + String.valueOf(totalLineOfResearchInProgressUndergraduateGuidance) + "\n\n\n";
         }
         
         output += String.valueOf(resumes.size());
@@ -143,7 +218,7 @@ public class OutputController {
         return output;
     }
     
-    private String countConferenceArticleLevel(String level, List<ConferenceArticle> articleList) {
+    private Integer countConferenceArticleLevel(String level, List<ConferenceArticle> articleList) {
         
         Integer count = 0;
         for(ConferenceArticle article : articleList) {
@@ -152,10 +227,10 @@ public class OutputController {
             }
         }
         
-        return count.toString();
+        return count;
     }
     
-    private String countJournalArticleLevel(String level, List<JournalArticle> articleList) {
+    private Integer countJournalArticleLevel(String level, List<JournalArticle> articleList) {
         
         Integer count = 0;
         for(JournalArticle article : articleList) {
@@ -164,6 +239,6 @@ public class OutputController {
             }
         }
         
-        return count.toString();
+        return count;
     }
 }
